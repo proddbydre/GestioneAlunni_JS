b84e1b064f0479f7bc4c972387ab57531506145c
lai2-24 <lai2-24@itismeucci.com>
2025-04-02 09:09:58 +0200

    gestiti i primi due casi delle certificazioni

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -4,12 +4,89 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class AlunniController
 {
+  
+
   public function index(Request $request, Response $response, $args){
+    $queryParams = $request -> getQueryParams();
+
+    $search = $queryParams['search'] ?? null;
+    $sortCol = $queryParams['sortCol'] ?? null;
+    $sort = $queryParams['sort'] ?? null;
+
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
-    $result = $mysqli_connection->query("SELECT * FROM alunni");
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE nome LIKE '%$search%' ORDER BY '$sortCol' '$sort'");
     $results = $result->fetch_all(MYSQLI_ASSOC);
 
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
+
+  public function getOne(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id=$id");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function createOne(Request $request, Response $response, $args){
+    $body = json_decode($request->getBody());
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $query = "INSERT INTO alunni (nome, cognome, codiceFiscale) VALUES ('$body->nome', '$body->cognome', '$body->codiceFiscale');";
+    $mysqli_connection->query($query) or die ('Unable to execute query. '. mysqli_error($query));
+    return $response->withStatus(200);
+  }
+
+  public function updateOne(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $body = json_decode($request->getBody());
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $query = "UPDATE alunni SET nome = '$body->nome', cognome= '$body->cognome', codiceFiscale = '$body->codiceFiscale' WHERE id = $id;";
+    $mysqli_connection->query($query) or die ('Unable to execute query. '. mysqli_error($query));
+    return $response->withStatus(200);
+  }
+
+  public function deleteOne(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $mysqli_connection->query("DELETE FROM alunni WHERE id='$id';") or die ('Unable to execute query. '. mysqli_error($query));
+    return $response->withStatus(200);
+  }
+
+  public function getAlunnoCert(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM certificazioni WHERE alunno_id=$id");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function getOneCert(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $c_id = $args['id_cert'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT *
+                                         FROM certificazioni c 
+                                         WHERE alunno_id=$id AND id=$c_id");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+
+  public function createCert(Request $request, Response $response, $args){
+    $body = json_decode($request->getBody());
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $query = "INSERT INTO certificazioni (id, alunno_id, titolo, votazione, ente) VALUES ('$body->id', '$body->alunno_id', '$body->titolo', '$body->votazione', '$body->ente');";
+    $mysqli_connection->query($query) or die ('Unable to execute query. '. mysqli_error($query));
+    return $response->withStatus(200);
+  }
+
+
+
 }
--- php/index.php
+++ php/index.php
@@ -6,6 +6,30 @@ require __DIR__ . '/controllers/AlunniController.php';
 
 $app = AppFactory::create();
 
+// -------- END POINT ALUNNI --------
+
+// Visualizzazione di tutti gli alunni e di uno solo
 $app->get('/alunni', "AlunniController:index");
+$app->get("/alunni/{id}", "AlunniController:getOne");
+
+// Creazione di un nuovo alunno: curl -X POST localhost:8080/alunni -d '{"nome":"Young","cognome":"Thug","codiceFiscale":"FREEY00UNGTHUG"}' -H "Content-Type: application/json"
+$app->post("/alunni", "AlunniController:createOne");
+
+// Aggiornamento di un alunno esistente: curl -X PUT localhost:8080/alunni/3 -d '{"nome":"Andrea","cognome":"Ajdinaj"}' -H "Content-Type: application/json"
+$app->put("/alunni/{id}", "AlunniController:updateOne");
+
+// Eliminazione un alunno esistente: curl -X DELETE localhost:8080/alunni/3 
+$app->delete("/alunni/{id}", "AlunniController:deleteOne");
+
+
+// -------- END POINT CERTIFICAZIONI --------
+
+// Visualizzazione di tutte le certificazioni di uno specifico alunno
+$app->get("/alunni/{id}/certificazioni", "AlunniController:getAlunnoCert");
+// Visualizzazione di una singola certificazione di uno specifico alunno
+$app->get("/alunni/{id}/certificazioni/{id_cert}", "AlunniController:getOneCert");
+
+// Creazione di una nuova certificazione per un alunno: curl -X POST localhost:8080/alunni/1/certificazioni -d '{"id":"4","alunno_id":"2","titolo":"C++","votazione":"81","ente":"Meucci"}' -H "Content-Type: application/json
+$app->post("/alunni/{id}/certificazioni", "AlunniController:createCert");
 
 $app->run();
